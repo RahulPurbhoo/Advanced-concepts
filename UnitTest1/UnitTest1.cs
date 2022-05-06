1f82fcf3c6d194108f1755f05c2d41017c3b0db9
RahulPurbhoo <99993620+RahulPurbhoo@users.noreply.github.com>
2022-05-06 12:19:14 +0200

    Unit Testing

---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            double Algebra = 81;
+            double Calculus = 79;
+            double Programming = 83;
+            double Databases = 89;
+            double Expected = 83;
+
+            //Act
+            double Actual = WindowsFormsApp_CourseAssessment.Form1.CalculateAverage(Algebra, Calculus, Programming, Databases);
+
+            //Assert
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
